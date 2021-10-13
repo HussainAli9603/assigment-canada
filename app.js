4ba4389cc7753c5787282bcef9970d490796a669
HussainAli9603 <72409657+HussainAli9603@users.noreply.github.com>
2021-10-13 09:44:14 +0500

    app

--- app.js
+++ app.js
@@ -4,20 +4,11 @@ express = require("express"),
   fs = require('fs'),
   app = require('express')();
 var http = require('http');
-var cookieParser = require('cookie-parser');
 var server = http.createServer(app);
 var exphbs = require('express-handlebars');
-var bodyParser = require('body-parser');
 var User = require('./user.json');
-const session = require('express-session');
 app.use(express.json())
 
-app.use(cookieParser());
-app.use(session({
-  secret: 'keyboard cat',
-  resave: true,
-  saveUninitialized: true
-}))
 app.set("view engine", "handlebars");
 app.engine("handlebars", exphbs({ layoutsDir: __dirname + "/views", extname: "handlebars", }));
 
@@ -25,29 +16,15 @@ app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.static(__dirname + '/public', { maxAge: '30 days' }));
 app.use('/uploads', express.static(__dirname + '/uploads'));
-const fileUpload = require('express-fileupload');
 
-app.use(fileUpload());
 app.use(express.json())
-app.use(bodyParser.json());
-// for parsing application/xwww-
-app.use(bodyParser.urlencoded({ extended: true }));
 
 
 app.get('/', function (req, res) {
   res.render('login')
 });
 app.get('/web-banking', function (req, res) {
-  console.log(req.session.user.u_name)
-  if (req.session && req.session.user && req.session.user.u_name) {
-    res.render('banking', {
-      user: {
-        u_name: req.session.user.u_name
-      }
-    })
-  } else {
-    res.redirect('/');
-  }
+    res.render('banking')   
 });
 
 app.post('/login', async (req, res) => {
@@ -64,16 +41,12 @@ app.post('/login', async (req, res) => {
   else {
 
     username = User.user.filter(x => x.username === req.body.username)
-    console.log(username[0].password)
     if (username[0].password === req.body.password) {
-      if (req.session) {
-        req.session.user = {
-          u_name: User.user.find(x => x.username == req.body.username)
-        }
-
-      }
-      req.session.save();
-      res.redirect('/web-banking');
+  
+      console.log(username)
+      res.render('banking',{
+        user:username[0]
+      });
     } else {
       errors.push({ message: 'Not a valid password' });
       if (errors.length > 0) {
@@ -88,7 +61,6 @@ app.post('/login', async (req, res) => {
 });
 
 app.get('/logout', function (req, res) {
-  req.session.destroy()
   res.redirect('/')
 });
 
